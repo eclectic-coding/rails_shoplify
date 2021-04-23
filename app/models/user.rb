9b4d6b0046bcbdfeb42d20d19e45781a84079222
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-04-23 16:19:41 -0400

    Create a customer stripe id on each new user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def to_s
+    email
+  end
+
+  after_create do
+    create_stripe_id
+  end
+
+  private
+
+  def create_stripe_id
+    customer = Stripe::Customer.create(email: email)
+    update(stripe_customer_id: customer.id)
+  end
 end
